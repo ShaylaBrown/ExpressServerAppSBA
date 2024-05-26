1039372b0137bc0b93c6f3fb77fd65fe7165ef01
ShaylaBrown <164940252+ShaylaBrown@users.noreply.github.com>
2024-05-26 17:21:45 -0500

    added additional routes, chain linked the last route, added middleware

--- index.js
+++ index.js
@@ -6,11 +6,45 @@ app.listen(port, () => {
     console.log(`Server is listening on port: ${port}.`);
 });
 
+const jsonParser = express.json();
+
+app.use(jsonParser);
+
+const logReq = (req, res, next) => {
+  console.log(`A ${req.method} request was made to ${req.url}`);
+  next();
+};
+
+app.use(logReq);
+app.use((err, req, res, next) => {
+  res.status(400).send(err.message);
+});
+
 app.get("/", (req, res) => {
-    res.send("Hello Express!");
-});
-
-app.get("express", (req, res) => {
-    res.send("Creating routes with Express is simple!");
-});
-
+    res.send("Try navigating to /user.");
+  });
+  
+  app.get("/user", (req, res) => {
+    res.send(`Received a GET request for user!
+  Try navigating to /user/somevalue/profile/somevalue.`);
+  });
+  
+  app.get("/user/:userID", (req, res) => {
+    res.send(`Navigated to the user page for: ${req.params.userID}.`);
+  });
+  
+  app.get("/user/:userID/profile", (req, res) => {
+    res.send(`Navigated to the user profile page for: ${req.params.userID}.`);
+  });
+  
+  app.route("/user/:userID/profile/:data") 
+  .get((req, res) => {
+    res.send(`Navigated to the user profile page for: ${req.params.userID}, with the data: ${req.params.data}.`);
+  })
+  .post((req, res) => {
+    res.send("Add a user.");
+  })
+  .patch((req, res) => {
+    res.send("Update the user's information.");
+  });
+  
